5a4c34fd00b06ced0973c160dbe61b24954f17b9
JoeYlMS <ivbazulyk@gmail.com>
2022-06-02 19:35:48 +0300

    apertur_carousel

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $('.slide-one').owlCarousel({
+        loop:true,
+        margin:50,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:4
+            }
+        }
+    });
+});
